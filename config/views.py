2b65c2cd400565314180f5b8e8e1825c5a3c1d0f
baek <dkf1912@gmail>
2022-02-20 20:51:45 +0900

    data_analysis - 로그인/회원가입 추가

--- config/views.py
+++ config/views.py
@@ -6,3 +6,7 @@ from start.models import *
 def home(request):
     print(request.user)
     return render(request, "main/index.html", {})
+
+def login(request):
+    print(request.user)
+    return render(request, "login/login_resist_form.html", {})
