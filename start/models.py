165001c91e017eb94a1501b30aa3a0790cce7adf
baek <dkf1912@gmail>
2022-02-27 23:55:49 +0900

    로그인/회원가입 v1.1 - 뼈대 생성

--- start/models.py
+++ start/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    nickname = models.CharField(max_length=64, verbose_name = '닉네임')
+    email = models.CharField(max_length=64, verbose_name = '이메일')
+    password = models.CharField(max_length=64, verbose_name = '비밀번호')
+
+    class Meta:
+        db_table = 'account'
+        app_label = 'start'
+        ordering = ['id']
+        managed = False
+
+    def __str__(self):
+        return '@@@' + self.name
--- start/views.py
+++ start/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.hashers import make_password
+from .models import User
 # Create your views here.
